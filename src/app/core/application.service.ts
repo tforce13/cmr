4c8d32bd07f1e78fa0f74079252a7b885211427d
William Best <william.best@outlook.com>
2018-03-28 16:45:38 -0600

    Saving loan application and contact info

--- src/app/core/application.service.ts
+++ src/app/core/application.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Application } from './../models/application';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 @Injectable()
 export class ApplicationService {
   uid: string;
   product: string;
 
-  constructor() {
+  constructor(
+    private afs: AngularFirestore
+  ) {
     this.uid = '';
     this.product = '';
    }
@@ -24,5 +28,10 @@ export class ApplicationService {
 
    setProduct (product: string) {
      this.product = product;
+   }
+
+   setApplicationData ( application: Application) {
+    const profileRef: AngularFirestoreDocument<Application> = this.afs.doc(`applications/${application.uid}`);
+      return profileRef.set(application);
    }
 }
--- src/app/pages/application/application.component.ts
+++ src/app/pages/application/application.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ReactiveFormsModule,  FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { ApplicationService } from './../../core/application.service';
+import { Application } from './../../models/application';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-application',
@@ -11,13 +13,15 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ApplicationComponent implements OnInit {
   uid: string; 
-  selectedProduct: string;
+  product: string;
   rates: Observable<any[]>;
   users: Observable<any[]>;
   profiles: Observable<any[]>;  
   applicationForm: FormGroup;
+  contacted: boolean;
 
   constructor(
+    public router: Router,
     public fb: FormBuilder,
     private afs: AngularFirestore,
     private applicationService: ApplicationService) {
@@ -27,43 +31,62 @@ export class ApplicationComponent implements OnInit {
      }
 
   ngOnInit() {
+    this.contacted = false;
     this.uid = this.applicationService.getUid();
-    this.selectedProduct = this.applicationService.getProduct();
+    this.product = this.applicationService.getProduct();
 
     console.log ("application");
     console.log ("uid: ", this.uid);
     console.log ("profile: ", this.product);
 
-    // this.applicationForm = this.fb.group({
-    //   firstName: ['', [ Validators.required ] ],
-    //   lastName:  ['', [ Validators.required ] ],
-    //   address1:  ['', [ Validators.required ] ],
-    //   address2:  ['', [ Validators.required ] ],
-    //   city:      ['', [ Validators.required ] ],
-    //   state:     ['', [ Validators.required ] ],
-    //   zip:       ['', [ Validators.required ] ],
-    //   email:     ['', [ Validators.required ] ],
-    //   phone:     ['', [ Validators.required ] ],
-    //   product:   ['', [ Validators.required ] ],
-    //   message:   ['', [ Validators.required ] ]
-    // });    
+     this.applicationForm = this.fb.group({
+       firstName: ['', [ Validators.required ] ],
+       lastName:  ['', [ Validators.required ] ],
+       address1:  ['', [ Validators.required ] ],
+       address2:  ['', [ Validators.required ] ],
+       city:      ['', [ Validators.required ] ],
+       state:     ['', [ Validators.required ] ],
+       zip:       ['', [ Validators.required ] ],
+       email:     ['', [ Validators.required ] ],
+       phone:     ['', [ Validators.required ] ],
+       message:   ['', [ Validators.required ] ]
+     });    
 
   }
 
-  // get firstName() { return this.applicationForm.get('firstName'); }
-  // get lastName()  { return this.applicationForm.get('lastName'); }
-  // get address1()  { return this.applicationForm.get('address1'); }
-  // get address2()  { return this.applicationForm.get('address2'); }
-  // get city()      { return this.applicationForm.get('city'); }
-  // get state()     { return this.applicationForm.get('state'); }
-  // get zip()       { return this.applicationForm.get('zip'); }
-  // get email()     { return this.applicationForm.get('email'); }
-  // get phone()     { return this.applicationForm.get('phone'); }
-  // get product()   { return this.applicationForm.get('product'); }
-  // get message()   { return this.applicationForm.get('message'); }
+   get firstName() { return this.applicationForm.get('firstName'); }
+   get lastName()  { return this.applicationForm.get('lastName'); }
+   get address1()  { return this.applicationForm.get('address1'); }
+   get address2()  { return this.applicationForm.get('address2'); }
+   get city()      { return this.applicationForm.get('city'); }
+   get state()     { return this.applicationForm.get('state'); }
+   get zip()       { return this.applicationForm.get('zip'); }
+   get email()     { return this.applicationForm.get('email'); }
+   get phone()     { return this.applicationForm.get('phone'); }
+   get message()   { return this.applicationForm.get('message'); }
 
-  onContact() {
+  setContact(uid) {
     console.log ('onContact');
+    let application: Application = {
+      uid:          this.uid,
+      firstName:    this.firstName.value,
+      lastName:     this.lastName.value,
+      address1:     this.address1.value,
+      address2:     this.address2.value,
+      city:         this.city.value,
+      state:        this.state.value,
+      zip:          this.zip.value,
+      email:        this.email.value,
+      phone:        this.phone.value,
+      product:      this.product,
+      message:      this.message.value
+    };
+    this.applicationService.setApplicationData(application);
+    this.contacted = true;
+  }
+  onContinue() {
+    console.log ('onContinue');
+    this.router.navigate(['']);
   }
 
 }
